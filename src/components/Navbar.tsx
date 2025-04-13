ba775031cf5f469183b6d1a7b8f1a62c0bb48ecb
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-04-13 16:26:11 +0300

    Setup shadcnui , added a navbar and a toggle to switch between light and dark mode

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Search, Bell, Menu, MessageSquare } from "lucide-react";
+import ThemeToggle from "./ThemeToggle";
+import { Button } from "./ui/button";
+import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto  h-15 flex items-center ">
+        <div className="flex items-center gap-2 md:gap-4">
+          <Link href="/" className="flex space-x-2">
+            <span className="font-bold text-xl text-primary">DevDiscuss</span>
+          </Link>
+        </div>
+
+        <div className="flex flex-1 items-center justify-center">
+          {/* //use md */}
+          <div className="relative w-full max-w-sm hidden md:flex mx-auto items-center">
+            <Search className="absolute left-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search..."
+              className="pl-8 bg-background w-full"
+            />
+          </div>
+          <div className="flex  items-center justify-end gap-3">
+            <ThemeToggle />
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <Bell className="h-5 w-5" />
+              <span className="sr-only">Notifications</span>
+            </Button>
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <MessageSquare className="h-5 w-5" />
+              <span className="sr-only">Messages</span>
+            </Button>
+            <Avatar className="h-8 w-8">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>SC</AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
